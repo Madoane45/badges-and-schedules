b6cb63cb1cf58ec8493b4eba4bad6a81e982ea8d
Madoane45 <Madoane45@gmail.com>
2021-02-05 23:01:41 -0500

    commit tree bb2c1b288f45d3e725bb28fa9979b59a8b6e99bd

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+require "pry"
+# Write your code here
+def badge_maker(name)
+   return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    new_names = []
+    names.each do |name|
+     new_names << "Hello, my name is #{name}."  
+    end
+    return new_names
+end
+
+def assign_rooms (speakers)
+     new_speakers = []
+     speakers.each.with_index (1) do |speaker, index|
+    new_speakers << "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    end
+ return new_speakers
+end
+def printer (cards)
+    batch_badge_creator(cards).each do |card|
+     
+      puts card
+   end
+    assign_rooms(cards).each do |card|
+        puts card
+    end 
+      #binding.pry
+end
